6a9744edab71b5303cb7881de589c7903384930e
Courtlyn Boykin <courtlynb@google.com>
2024-08-05 14:46:04 -0400

    added skin type selectors and dynamic photos and text descriptions for each skin type

--- src/app/components/matcher/matcher.component.ts
+++ src/app/components/matcher/matcher.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-matcher',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './matcher.component.html',
   styleUrl: './matcher.component.css'
 })
 export class MatcherComponent {
+  selectedSkinType: string = '';
+  skinTypeImages = {
+    dry: "assets/dry-skin.png",
+    oily: "assets/oily-skin.png",
+    combination: 'assets/combination-skin.png',
+    normal: 'assets/normal-skin.png',
+    sensitive: 'assets/sensitive-skin.png'
+  };
+  defaultImage = 'assets/logo.png';
+  skinTypeDescriptions = {
+    dry: 'Description for dry skin',
+    oily: 'Description for oily skin',
+    combination: 'Description for combination skin',
+    normal: 'Description for normal skin',
+    sensitive: 'Description for sensitive skin'
+  };
 
+  onSkinTypeSelected(skinType: string){
+    this.selectedSkinType = skinType
+  }
 }
